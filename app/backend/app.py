8f70cfd1543b5da762978b7982a4ba3c66542416
Duda Lucena Miguel <duda.lucena@outlook.com>
2025-10-24 18:05:16 -0300

    commit

--- app/backend/app.py
+++ app/backend/app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_cors import CORS
 from routes.usuarioRoutes import usuario_bp
 from routes.cicloRoutes import ciclo_bp
 from routes.sessaoRoutes import sessao_bp
@@ -7,6 +8,8 @@ from routes.temaRoutes import tema_bp
 
 
 app = Flask(__name__)
+CORS(app)
+
 app.register_blueprint(usuario_bp)
 app.register_blueprint(ciclo_bp)
 app.register_blueprint(sessao_bp)
